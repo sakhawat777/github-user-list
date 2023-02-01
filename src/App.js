778c403b224a37b67630df2d7e1a663c8d281fd2
sakhawat777 <info@vumi.com.bd>
2023-02-01 15:58:31 +0600

    Practice done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import GithubUsers from "./components/GithubUsers";
+
+function App() {
+  return (
+    <div>
+      <GithubUsers />
+    </div>
+  );
+}
+
+export default App;
